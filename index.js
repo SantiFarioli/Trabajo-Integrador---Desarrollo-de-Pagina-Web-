db9f7039f3b5c59d3ef85a2ec0a86baead45efd3
Santiago Farioli <santiago8773cba@gmail.com>
2023-05-10 15:58:19 -0300

    agregando

---
+++ index.js
@@ -0,0 +1,6 @@
+/*let nombreCompleto;
+let edad;
+nombre = prompt ("Digite su nombre completo: ");
+edad = prompt ("Digite su edad: ");
+alert("Su nombre  es: " + nombre);
+alert("Su edad es: " + edad);*/
